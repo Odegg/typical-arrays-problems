fd860f0e580947b656a2ea15e0f96f3716b54ba9
Adam Petrashkevich <adasechkaa@gmail.com>
2020-09-30 15:06:48 +0300

    fix: fix error

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ exports.min = function min (array) {
   if (typeof array !== 'undefined' && array.length > 0)
     return Math.min.apply(null, array);
   return 0;
-};
+}
 
 exports.max = function max (array) {
   if (typeof array !== 'undefined' && array.length > 0)
     return Math.max.apply(null, array);
   return 0;
-};
+}
 
 exports.avg = function avg (array) {
   if (typeof array !== 'undefined' && array.length > 0)
-    return array.reduce((sum, current) => sum + current)/ array.length;
+    return array.reduce((sum, current) => sum + current) / array.length;
   return 0;
-};
+}
